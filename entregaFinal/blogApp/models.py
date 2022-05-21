ae2a47640fbcd9872f33c89a2b789a2e9c03102b
Martin Aristov <aristovmartin@gmail.com>
2022-05-21 15:07:22 -0300

    Modelos iniciales creados
    Se crearon los modelos iniciales, pero aun no se migro la bd

--- entregaFinal/blogApp/models.py
+++ entregaFinal/blogApp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    titulo = models.CharField(max_length=40)
+    subtitulo = models.CharField(max_length=40)
+    cuerpo = models.CharField(max_length=250)
+    autor = models.CharField(max_length=40)
+    fecha = models.DateField()
+    #falta ver como poner imagen
+    
+    def __str__(self):
+        return self.titulo + " " + self.autor + " " + str(self.fecha)
+    
+class Usuario(models.Model):
+    email = models.EmailField()
+    contrasena = models.CharField(max_length=20)
+    nombre_usuario = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.nombre_usuario + " " + self.email
+
