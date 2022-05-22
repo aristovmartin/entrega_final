44d43f22d2ec140962c36f56070aba4d33b30f3c
Martin Aristov <aristovmartin@gmail.com>
2022-05-22 17:17:37 -0300

    Ajustes main.html
    Se agrego un pequeno ajuste a main.html para que si el dia que se publica un blog es el dia de hoy, se muestra un cartel que dice "Nuevo".
    Ademas, se intento pasar el id del blog a la pagina donde luego se mostrara, pero con errores.

--- entregaFinal/blogApp/urls.py
+++ entregaFinal/blogApp/urls.py
@@ -4,6 +4,6 @@ from blogApp import views
 urlpatterns = [
     path('', views.main, name="inicio"),
     path('about', views.about_me, name="about"),
-    path('blog', views.pagina_blog, name="pagina_blog"),
+    path('blog/<id>', views.pagina_blog, name="pagina_blog"),
 
 ]
--- entregaFinal/blogApp/views.py
+++ entregaFinal/blogApp/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from .models import *
 from .forms import *
+from datetime import *
 
 # Create your views here.
 
 def about_me(request):
     return render(request,'about_me.html')
 
-def pagina_blog(request):
-    return render(request,'page.html')
+def pagina_blog(request,id):
+    blog = Blog.objects.get(id_blog = id)
+    today = datetime.now()
+    return render(request,'page_blog.html',{"blog":blog})
 
 #muestro los blogs en el main
 def main(request):
     blogs = Blog.objects.all()
-    return render(request,'main.html',{"blogs":blogs})
+    today = datetime.now().date()
+    return render(request,'main.html',{"blogs":blogs,"fecha_hoy":today})
