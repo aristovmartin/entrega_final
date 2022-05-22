b0ae608a7d2adfc84b1cad46e8056adf7594235b
Martin Aristov <aristovmartin@gmail.com>
2022-05-22 15:31:56 -0300

    Cambios menores
    Se agrego una imagen generica para probar como se mostraban los blogs listados en main, ademas se creo el modelo Mensajes para usar en la app de mensajeria.

--- entregaFinal/blogApp/models.py
+++ entregaFinal/blogApp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
+    id_blog = models.BigAutoField(primary_key=True)
     titulo = models.CharField(max_length=40)
     subtitulo = models.CharField(max_length=40)
     cuerpo = models.CharField(max_length=250)
@@ -13,10 +14,16 @@ class Blog(models.Model):
         return self.titulo + " " + self.autor + " " + str(self.fecha)
     
 class Usuario(models.Model):
+    id_usuario = models.BigAutoField(primary_key=True)
     email = models.EmailField()
     contrasena = models.CharField(max_length=20)
     nombre_usuario = models.CharField(max_length=30)
     
     def __str__(self):
         return self.nombre_usuario + " " + self.email
+    
+class Mensaje(models.Model):
+    id_usuario_origen = models.IntegerField()
+    id_usuario_destino = models.IntegerField()
+    texto = models.CharField(max_length=250)
 
